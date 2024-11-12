5bfe92321261d1be8b29691d90137669a007affd
cruzgraziele <142243237+cruzgraziele@users.noreply.github.com>
2024-11-12 09:57:31 -0300

    Add files via upload

--- graziii-main/graziii-main/perguntas.js
+++ graziii-main/graziii-main/perguntas.js
@@ -15,3 +15,11 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+
+criaCartao(
+ 
+    'Série',
+    'Qual o nome da série',
+    'The Walking Dead',
+)
